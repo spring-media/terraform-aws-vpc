3af350d3fc44be4b506e2d16ce1f469241172e7a
Dennis Kern <dennis.kern@axelspringer.com>
2025-07-07 14:27:05 +0200

    remove debug code

--- outputs.tf
+++ outputs.tf
@@ -136,10 +136,6 @@ output "public_route_table_ids" {
   value       = local.public_route_table_ids
 }
 
-# output "public_internet_gateway_route_id" {
-#   description = "ID of the internet gateway route"
-#   value       = try(aws_route.public_internet_gateway[0].id, null)
-# }
 
 output "public_internet_gateway_ipv6_route_id" {
   description = "ID of the IPv6 internet gateway route"

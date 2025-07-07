0df9b047b58eec89f49274a445e9553c55462496
Fabio Lisco <fabio.lisco@axelspringer.com>
2025-07-07 09:48:58 +0200

    fix variable name

--- main.tf
+++ main.tf
@@ -335,7 +335,7 @@ resource "aws_subnet" "gwlb" {
   availability_zone = each.value.az
 
   tags = {
-    Name = "${team_name}-${var.environment_name}-${var.short_aws_region}-${each.value.az}-sub-gwlb"
+    Name = "${var.team_name}-${var.environment_name}-${var.short_aws_region}-${each.value.az}-sub-gwlb"
   }
   depends_on = [aws_vpc_ipv4_cidr_block_association.secondary_cidr]
 }
